d35bf9d1b0836021b16024117d17391d8a74b373
JERRYKWAK\junsa <junsang0129@gmail.com>
2025-07-27 17:25:08 +0900

    로컬 브랜치 생성 및 커밋
    main 로컬 브랜치를 세이브 포인트로 쓰기 위해 작업 내용은 Feature 로컬 브랜치로 따로 파서 진행하는 방법을 배웠다.
    새 로컬 브랜치에서 작업 후 main 로컬 브랜치로 머지하는 연습을 해봤다.

--- Source/TeamProject/MyActor.cpp
+++ Source/TeamProject/MyActor.cpp
@@ -7,21 +7,6 @@
 AMyActor::AMyActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
-
-// Called when the game starts or when spawned
-void AMyActor::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AMyActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
